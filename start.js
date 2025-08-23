d30d14143d6396008eeeed079fc978906093b812
Mike Hartford <mikehartford25@gmail.com>
2025-08-23 02:33:06 -0400

    Force Railway to run server only - remove build script, add start.js

---
+++ start.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+// Simple start script for Railway
+console.log('🚂 Starting Egypt MMO Server...');
+
+// Start the server
+require('./server/index.js');
+
+console.log('✅ Server started successfully');
